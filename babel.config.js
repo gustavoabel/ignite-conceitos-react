38b0f2f4d1fc8e5ccf578c990a54260b1ca1bddd
gustavoabell <gustavoabel.contato@gmail.com>
2021-05-02 12:41:10 -0300

    finish chapter One

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,12 @@
 module.exports = {
-    presets: [
-        '@babel/preset-env',
-        '@babel/preset-react'
-    ]
-}
+  presets: [
+    "@babel/preset-env",
+    "@babel/preset-typescript",
+    [
+      "@babel/preset-react",
+      {
+        runtime: "automatic",
+      },
+    ],
+  ],
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import { Counter } from './components/Counter';
 import { RepositoryList } from './components/RepositoryList';
 import './styles/global.scss';
 
 export function App() {
-  return ( 
-    <>
+  return (  
   <RepositoryList />
-  <Counter />
-  </>
-  );
+    );
 }
